33bf8cdf1a1077cc2eaf2846b70799ee18c3b4b6
Ulrik Sandberg <ulrik.sandberg@jayway.com>
2011-06-15 21:44:13 +0200

    Added exclusions. Reverted message.

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
 		 [ring/ring-core "0.3.8"]
 		 [ring/ring-jetty-adapter "0.3.8"]]
-  :dev-dependencies [[lein-ring "0.4.3"]]
-  :ring {:handler cljheroku.core/app})
+  :exclusions [org.mortbay.jetty/servlet-api])
--- src/cljheroku/core.clj
+++ src/cljheroku/core.clj
@@ -1,9 +1,9 @@
 (ns cljheroku.core
   (:use ring.util.response
-        ring.adapter.jetty))
+	ring.adapter.jetty))
 
 (defn app [req]
-  (response "Hello World, I tell you!"))
+  (response "Hello world"))
 
 (defn -main []
   (let [port (Integer/parseInt (get (System/getenv) "PORT" "8080"))]
